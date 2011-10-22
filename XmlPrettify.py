9a9c6b3bf51c4609ed9024c8411afbebdacc649e
Budd <d.budaragin@gmail.com>
2011-10-23 01:37:01 +0400

    added xml parser exception handling; fixed issue with consequent prettifies;

--- XmlPrettify.py
+++ XmlPrettify.py
@@ -2,8 +2,13 @@ import sublime, sublime_plugin
 
 class XmlPrettifyCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		from xml.dom.minidom import parseString
+		import xml.dom.minidom
+		import xml.parsers.expat
 		selection = self.view.sel()[0]
-		dom = parseString(self.view.substr(selection).encode('utf-8'))
-		prettified = dom.toprettyxml()
+		try:
+			text = self.view.substr(selection).encode('utf-8')
+			dom = xml.dom.minidom.parseString(text.replace('\n', '').replace('\t', ''))
+			prettified = dom.toprettyxml()
+		except xml.parsers.expat.ExpatError:
+			return
 		self.view.replace(edit, selection, prettified)
